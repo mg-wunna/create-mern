77b43e3359ee041d6d44de37e1f58a1765513d1e
wunna <mail@wunna.info>
2022-07-01 22:26:32 +0630

    create express server and middleware & listen server at the port

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 import {} from 'dotenv/config';
 import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
 
 // ✔ connect with mongo database
-const { MONGO_URL } = process.env;
+const { MONGO_URL, PORT } = process.env;
 mongoose
   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(_ => {
     console.log('Mongo database connected.');
+
+    // ✔ create express server and middleware
+    const server = express();
+    server.use(express.urlencoded({ extended: true }));
+    server.use(express.json());
+    server.use(cors());
+
+    // ✔ listen server at the port
+    server.listen(PORT, console.log(`Server is listening at the port ${PORT}!`));
   })
   .catch(error => {
     console.log('Mongo database connection error.');
